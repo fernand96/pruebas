b85c7d961647f5a6c1fd25f5f80a4eca9b994087
sairy7 <71484435+sairy7@users.noreply.github.com>
2021-02-14 01:08:58 -0600

    actualizacion de archivo login.php

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>ME VAS A EXTRAÑAS</H1>
+    <P>me vas a extrañar te apuesto que vas a buscarme y vas a llorar porque a mi 
+     jamas supiste valorarme</P>
+     <p>pero te juro que me vas a extrañar chiquitito </p>
+</body>
+</html>
