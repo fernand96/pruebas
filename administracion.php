09ddefbd1aab803222cd1ca6f13568780f3e51f3
Fernando <fernandounah.rodriguez@gmail.com>
2021-02-14 02:03:57 -0600

    Modificacion de varios archivos

---
+++ administracion.php
@@ -0,0 +1,10 @@
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam voluptas tempora, sint officiis voluptatem quasi? Porro, repudiandae animi similique itaque, eius ad perferendis veniam, fuga officia sint error sunt iste.
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla sint unde ducimus numquam repudiandae vitae consequatur. Odio doloribus sequi voluptates architecto dicta earum quis obcaecati quod. Perferendis amet iusto perspiciatis!
+
+locale_accept_from_http
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse animi aperiam voluptate numquam! Accusantium esse similique recusandae, eligendi quo magni, sit alias delectus nobis illum repellat perspiciatis reiciendis incidunt! Non.
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde deserunt quis non provident assumenda accusantium ab accusamus doloremque nihil vel labore ex, quisquam sapiente reprehenderit, at consequatur corporis fugiat a!
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eveniet quaerat eius est animi natus repellat iure expedita architecto soluta optio, repudiandae quis tempore. Explicabo nam nisi ad adipisci commodi!
